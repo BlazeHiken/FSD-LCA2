705963eb641633d7069a021b6789992402f7103a
Siddharth Phadtare <siddharthphadtare@gmail.com>
2026-09-25 20:49:24 +0530

    added React Counter App using useState

---
+++ assignment6/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="app">
+      <div className="counter">
+        <h1>Counter App</h1>
+
+        <div className="count">{count}</div>
+
+        <div className="buttons">
+          <button onClick={() => setCount(count + 1)}>Increment</button>
+
+          <button onClick={() => setCount(count - 1)}>Decrement</button>
+
+          <button onClick={() => setCount(0)}>Reset</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
